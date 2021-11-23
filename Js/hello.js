960ad2f9dce02cc8b74a81fa2bd7d6d91cc8139d
starforce21 <jackjack1214@gmail.com>
2021-11-23 14:28:59 -0500

    js

---
+++ Js/hello.js
@@ -0,0 +1,16 @@
+console.log("Hello Jack")
+num1=1
+num2=2
+num3=3
+num4=4
+num5=5
+add=num5+num4
+sub=num4-num3
+multi=num3*num2
+div=num2/num1
+avg=(num1+num2+num3+num4+num5)/5
+console.log(add)
+console.log(sub)
+console.log(multi)
+console.log(div)
+console.log(avg)
