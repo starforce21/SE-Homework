4775d389d57c5a5a3b79994debae18d2cd04a635
starforce21 <jackjack1214@gmail.com>
2021-12-04 21:21:47 -0500

    Update dec3.js

--- Js/dec3.js
+++ Js/dec3.js
@@ -16,6 +16,27 @@
 // newStr===input ? console.log(input+' is Palindrome'):console.log(input+' is not Palindrome');
 
 
+// let arr=[]
+// value=2
+// for (let r = 0; r < 5; r++) {
+//     arr[r]=[]  // making the row an array // arr was an array [a, a,a] then it become [[a, a, a],a,a,a]
+//     for (let c = 0; c < 6; c++) {
+//         arr[r][c]=value
+//         value+=2
+//     } 
+// }
+// console.log(arr);
 
-let str='hello'
-(str === '') ? '' : reverseString(str.substr(1)) + str.charAt(0);
+// numarray = []
+// value = 2
+// for(let x = 0; x < 5; x++){
+//     numarray[x] = []
+//     for(let y = 0; y < 6; y++){
+//         numarray[x][y] = []
+//         for(let z = 0; z < 7; z++){
+//             numarray[x][y][z] = value += 2
+//         }
+//     }
+// }
+// console.log(numarray)
+// console.log(numarray[0][0][0])
