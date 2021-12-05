d62c6db100d4e3334e4eaca1151999105c9b7432
starforce21 <jackjack1214@gmail.com>
2021-12-04 21:37:56 -0500

    update

--- Js/w2hw/q1.js
+++ Js/w2hw/q1.js
@@ -1,6 +1,4 @@
 const prompt= require("prompt-sync")()
 let input=prompt('Input is ')
-let input2=input.toLowerCase()
 let newStr=input.split("").reverse().join("");
-let input3=newStr.toLowerCase()
-input2===input3 ? console.log(input+' is Palindrome'):console.log(input+' is not Palindrome');
+input.toLowerCase()===newStr.toLowerCase() ? console.log(input+' is Palindrome'):console.log(input+' is not Palindrome');
